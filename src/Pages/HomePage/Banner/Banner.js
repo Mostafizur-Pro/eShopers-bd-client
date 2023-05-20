a912014595f4128bcec4005ad35400177b890c72
Mostafizur-pro <mostafizur0195@gmail.com>
2023-05-20 22:45:36 +0600

    home page banner add

---
+++ src/Pages/HomePage/Banner/Banner.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import images1 from '../../../assete/Banner/images1.jpg'
+import images3 from '../../../assete/Banner/images2.png'
+import images2 from '../../../assete/Banner/images3.jpg'
+
+const Banner = () => {
+    return (
+        <div className='my-20'>
+            <section className='grid grid-cols-3 gap-5 h-full'>
+                <div className='col-span-1 '>
+                <div className="hero min-h-full" style={{ backgroundImage: `url(${images1})`}}>
+  <div className="hero-overlay bg-opacity-10"></div>
+  <div className="hero-content  text-neutral-content">
+    <div className="max-w-md text-black my-16">
+    <p className="">NEW ARRIVALS</p>
+      <h1 className="mb-5 text-3xl font-bold">TOTAL BODY WASH</h1>
+      <p className="mb-5">Top Quality Item Skincare</p>
+      <button className="btn btn-outline">SHOP NOW</button>
+    </div>
+  </div>
+</div>
+                </div>
+                <div className='col-span-1'>
+                <div className="hero min-h-full" style={{ backgroundImage: `url(${images2})`}}>
+  <div className="hero-overlay bg-opacity-50"></div>
+  <div className="hero-content  text-neutral-content">
+    <div className="max-w-md  my-16">
+    <p className="">BLACK FRIDAY</p>
+      <h1 className="mb-5 text-3xl font-bold">DISCOUNT MASK</h1>
+      <p className="mb-5">Accusamus et iusto odgnissimos ducimusblanditiis praesentium </p>
+      <button className="btn btn-outline text-white">SHOP NOW</button>
+    </div>
+  </div>
+</div>
+                </div>
+                <div className='col-span-1'>
+                <div className="hero min-h-full" style={{ backgroundImage: `url(${images3})`}}>
+  <div className="hero-overlay bg-opacity-10"></div>
+  <div className="hero-content  text-neutral-content">
+    <div className="max-w-md text-black my-16">
+    <p className="">BESTSELLER ITEM</p>
+      <h1 className="mb-5 text-3xl font-bold">FACIAL CREAM</h1>
+      <p className="mb-5">Top Quality Item Skincare</p>
+      <button className="btn btn-outline">SHOP NOW</button>
+    </div>
+  </div>
+</div>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/HomePage/Home/Home.js
+++ src/Pages/HomePage/Home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import useTitle from "../../../hooks/useTitle/useTitle";
 import Sliderbanner from "../SliderBanner/Sliderbanner";
 import TopSellingProduct from "../TopSellingProduct/TopSellingProduct";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
   useTitle("Home");
   return (
     <div>
       <Sliderbanner />
+      <Banner />
       <TopSellingProduct />
     </div>
   );
