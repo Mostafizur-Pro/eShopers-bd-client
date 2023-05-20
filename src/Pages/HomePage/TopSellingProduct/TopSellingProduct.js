8d9099107d16c2799d34a916035958288e7f6abe
Mostafizur-pro <mostafizur0195@gmail.com>
2023-05-20 23:53:01 +0600

    Top Selling Products add

--- src/Pages/HomePage/TopSellingProduct/TopSellingProduct.js
+++ src/Pages/HomePage/TopSellingProduct/TopSellingProduct.js
@@ -10,7 +10,7 @@ import image08 from "../../../assete/TopSelling/08.webp";
 
 const TopSellingProduct = () => {
   return (
-    <div>
+    <div className="mb-20">
       <div className="text-center my-10">
         <h1 className="text-4xl font-semibold">Top Selling Products</h1>
         <p className="text-lg mt-2">
@@ -18,30 +18,62 @@ const TopSellingProduct = () => {
           primis eget moecenos
         </p>
       </div>
-      <div className="carousel ">
-        <div className="carousel-item mr-10">
-          <img className="w-3/6" src={image01} alt="Drink" />
+      <div className="grid grid-cols-4 gap-5">
+        <div className="col-span-1 mr-10">
+          <img className="w-full" src={image01} alt="Drink" />
+           <div className="text-center">
+         <h2>01106 - Upholstered Simple</h2>
+          <p className="text-gray-400">$93.00</p>
+         </div>
         </div>
-        <div className="carousel-item mr-10 ">
-          <img className="w-3/6" src={image02} alt="Drink" />
+        <div className="col-span-1 mr-10 ">
+          <img className="w-full" src={image02} alt="Drink" />
+           <div className="text-center">
+         <h2>01106 - Upholstered Simple</h2>
+          <p className="text-gray-400">$93.00</p>
+         </div>
         </div>
-        <div className="carousel-item mr-10">
-          <img className="w-3/6" src={image03} alt="Drink" />
+        <div className="col-span-1 mr-10">
+          <img className="w-full" src={image03} alt="Drink" />
+           <div className="text-center">
+         <h2>01106 - Upholstered Simple</h2>
+          <p className="text-gray-400">$93.00</p>
+         </div>
         </div>
-        <div className="carousel-item mr-10">
-          <img className="w-3/6" src={image04} alt="Drink" />
+        <div className="col-span-1 mr-10">
+          <img className="w-full" src={image04} alt="Drink" />
+           <div className="text-center">
+         <h2>01106 - Upholstered Simple</h2>
+          <p className="text-gray-400">$93.00</p>
+         </div>
         </div>
-        <div className="carousel-item mr-10">
-          <img className="w-3/6" src={image05} alt="Drink" />
+        <div className="col-span-1 mr-10">
+          <img className="w-full" src={image05} alt="Drink" />
+           <div className="text-center">
+         <h2>01106 - Upholstered Simple</h2>
+          <p className="text-gray-400">$93.00</p>
+         </div>
         </div>
-        <div className="carousel-item mr-10">
-          <img className="w-3/6" src={image06} alt="Drink" />
+        <div className="col-span-1 mr-10">
+          <img className="w-full" src={image06} alt="Drink" />
+          <div className="text-center">
+         <h2>01106 - Upholstered Simple</h2>
+          <p className="text-gray-400">$93.00</p>
+         </div>
         </div>
-        <div className="carousel-item mr-10">
-          <img className="w-3/6" src={image07} alt="Drink" />
+        <div className="col-span-1 mr-10">
+          <img className="w-full" src={image07} alt="Drink" />
+          <div className="text-center">
+         <h2>01106 - Upholstered Simple</h2>
+          <p className="text-gray-400">$93.00</p>
+         </div>
         </div>
-        <div className="carousel-item mr-10">
-          <img className="w-3/6" src={image08} alt="Drink" />
+        <div className="col-span-1 mr-10">
+          <img className="w-full" src={image08} alt="Drink" />
+         <div className="text-center">
+         <h2>01106 - Upholstered Simple</h2>
+          <p className="text-gray-400">$93.00</p>
+         </div>
         </div>
       </div>
     </div>
